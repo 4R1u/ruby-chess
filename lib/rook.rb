c1d6e2670faf597088604a347e6a42c3c05b6f7b
4R1u <62248461+4R1u@users.noreply.github.com>
2025-01-08 07:13:07 +0400

    feat: Rook < Piece

---
+++ lib/rook.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+
+# Pawn (should implement normal moves,
+# as well as moving two squares at the beginning, capturing and enpassant)
+class Rook < Piece
+  attr_accessor :moven
+
+  def initialize(black: false)
+    super(black ? '♜' : '♖', '')
+    @moven = false
+  end
+end
