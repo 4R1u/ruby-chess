efa3c2ed6f49cf2e277433377730c6d2c04d0d1a
4R1u <62248461+4R1u@users.noreply.github.com>
2025-01-08 06:51:48 +0400

    refactor!(piece, game): remove Piece#valid_move?
    Make it the Game's responsibility.

--- lib/piece.rb
+++ lib/piece.rb
@@ -6,9 +6,4 @@ class Piece
     @char = char
     @letter = letter
   end
-
-  def valid_move?(source, destination)
-    source.all? { |num| (0..7).cover?(num) } &&
-      destination.all? { |num| (0..7).cover?(num) }
-  end
 end
