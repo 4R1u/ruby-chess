fb325a12c3397cf128fcccc9c2e4d0e485a00c07
4R1u <62248461+4R1u@users.noreply.github.com>
2025-03-10 11:21:55 +0400

    refactor(pawn): make internal functions private

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -25,38 +25,42 @@ class Pawn < Piece
     end
   end
 
-  def self.source_one_square_behind(dst, game)
-    src = [dst[0] + game.backwards, dst[1]]
-    src if game.board.pawn?(src) && game.friend?(src)
-  end
+  class << self
+    private
 
-  def self.source_two_squares_behind(dst, game)
-    src = [dst[0] + (2 * game.backwards), dst[1]]
+    def source_one_square_behind(dst, game)
+      src = [dst[0] + game.backwards, dst[1]]
+      src if game.board.pawn?(src) && game.friend?(src)
+    end
 
-    src if game.board.pawn?(src) && !game.board.info_at(src, :moven?) &&
-           game.friend?(src) && game.board.empty?([src[0] + game.forwards, src[1]])
-  end
+    def source_two_squares_behind(dst, game)
+      src = [dst[0] + (2 * game.backwards), dst[1]]
 
-  def self.capturing_pawn(dst, file_number, game)
-    return unless (0..7).cover?(file_number)
+      src if game.board.pawn?(src) && !game.board.info_at(src, :moven?) &&
+             game.friend?(src) && game.board.empty?([src[0] + game.forwards, src[1]])
+    end
 
-    src = [dst[0] + game.backwards, file_number]
-    src if game.board.pawn?(src) && game.friend?(src) &&
-           (dst[1] - file_number).abs == 1 &&
-           game.enemy?(dst)
-  end
+    def capturing_pawn(dst, file_number, game)
+      return unless (0..7).cover?(file_number)
 
-  def self.en_passant_source(dst, file_number, game)
-    src = [dst[0] + game.backwards, file_number]
-    removed = [src[0], dst[1]]
+      src = [dst[0] + game.backwards, file_number]
+      src if game.board.pawn?(src) && game.friend?(src) &&
+             (dst[1] - file_number).abs == 1 &&
+             game.enemy?(dst)
+    end
 
-    return unless game.board.valid_coords?(src) &&
-                  game.board.valid_coords?(dst) && game.enemy?(removed)
+    def en_passant_source(dst, file_number, game)
+      src = [dst[0] + game.backwards, file_number]
+      removed = [src[0], dst[1]]
 
-    if game.board.pawn?(src) && game.friend?(src) &&
-       (dst[1] - src[1]).abs == 1
-      game.board.remove_piece(removed)
-      src
+      return unless game.board.valid_coords?(src) &&
+                    game.board.valid_coords?(dst) && game.enemy?(removed)
+
+      if game.board.pawn?(src) && game.friend?(src) &&
+         (dst[1] - src[1]).abs == 1
+        game.board.remove_piece(removed)
+        src
+      end
     end
   end
 end
