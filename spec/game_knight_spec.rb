ebbe339a2bc75d413e692917fddcca8935ecec28
4R1u <62248461+4R1u@users.noreply.github.com>
2025-03-15 16:47:04 +0400

    test: knight using x notation wrongly

--- spec/game_knight_spec.rb
+++ spec/game_knight_spec.rb
@@ -5,10 +5,30 @@ require_relative '../lib/game'
 describe Game do
   subject(:game) { described_class.new }
 
-  context 'when knight moves' do
-    context 'when moving 2 squares up and left' do
+  context 'when moving 2 squares up and left, jumping over a piece' do
+    before do
+      game.move 'Nc3'
+    end
+
+    it 'source is empty' do
+      expect(game.board.board[7][1].piece).to be_nil
+    end
+
+    it 'destination has knight' do
+      expect(game.board.board[5][2].piece).to be_a(Knight)
+    end
+  end
+
+  context 'when capturing' do
+    context 'with x' do
       before do
-        game.move 'Nc3'
+        game.move 'h4'
+        game.move 'a5'
+        game.move 'h5'
+        game.move 'a4'
+        game.move 'h6'
+        game.move 'a3'
+        game.move 'Nxa3'
       end
 
       it 'source is empty' do
@@ -16,8 +36,42 @@ describe Game do
       end
 
       it 'destination has knight' do
-        expect(game.board.board[5][2].piece).to be_a(Knight)
+        expect(game.board.board[5][0].piece).to be_a(Knight)
       end
+    end
+
+    context 'without x' do
+      before do
+        game.move 'h4'
+        game.move 'a5'
+        game.move 'h5'
+        game.move 'a4'
+        game.move 'h6'
+        game.move 'a3'
+        game.move 'Na3'
+      end
+
+      it 'source is empty' do
+        expect(game.board.board[7][1].piece).to be_nil
+      end
+
+      it 'destination has knight' do
+        expect(game.board.board[5][0].piece).to be_a(Knight)
+      end
+    end
+  end
+
+  context 'when trying to capture an empty square' do
+    before do
+      game.move 'Nxc3'
+    end
+
+    it 'source has knight' do
+      expect(game.board.board[7][1].piece).to be_a(Knight)
+    end
+
+    it 'destination is empty' do
+      expect(game.board.board[5][2].piece).to be_nil
     end
   end
 end
