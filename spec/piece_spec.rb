b2251758cf4b795bb971d0c6c0d29ecbb1a3466f
4R1u <62248461+4R1u@users.noreply.github.com>
2025-03-21 15:58:49 +0400

    test: use multiple pices in the same context

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -6,21 +6,20 @@ describe Piece do
   subject(:piece) { described_class }
 
   describe '::find_qualifier' do
-    context 'with a Rook' do
-      it 'returns file' do
-        expect(piece.find_qualifier('Rab3')).to eq('a')
-      end
-
-      it 'returns rank' do
-        expect(piece.find_qualifier('R1b3')).to eq('1')
-      end
-
-      it 'returns rank and file' do
-        expect(piece.find_qualifier('Ra1b3')).to eq([7, 0])
-      end
+    it 'returns file' do
+      expect(piece.find_qualifier('Rab3')).to eq('a')
     end
 
-    context 'with a Knight'
-    context 'with a Bishop'
+    it 'returns rank' do
+      expect(piece.find_qualifier('N1b3')).to eq('1')
+    end
+
+    it 'returns rank and file' do
+      expect(piece.find_qualifier('Ba1b3')).to eq([7, 0])
+    end
+
+    it 'returns nil with no qualifier' do
+      expect(piece.find_qualifier('Rc4')).to be_nil
+    end
   end
 end
