7514e3f35ff99730627447abb6a3a419784cd984
4R1u <62248461+4R1u@users.noreply.github.com>
2025-06-25 21:53:26 +0400

    feat: Rook::legal_moves

--- lib/rook.rb
+++ lib/rook.rb
@@ -17,6 +17,13 @@ class Rook < Piece
     uniq[1] if uniq.length == 2
   end
 
+  def self.legal_moves(src, game)
+    legal_moves_up(src, game) +
+      legal_moves_right(src, game) +
+      legal_moves_down(src, game) +
+      legal_moves_left(src, game)
+  end
+
   def self.source_left(dst, game, qualifier, type)
     ((dst[1] + 1)..7).each do |col|
       coords = [dst[0], col]
@@ -51,5 +58,65 @@ class Rook < Piece
       return nil unless game.board.empty?(coords)
     end
     nil
+  end
+
+  def self.legal_moves_up(src, game)
+    result = []
+    (1..7).each do |i|
+      if !game.board.valid_coords?([src[0] - i, src[1]])
+        break
+      elsif game.board.empty?([src[0] - i, src[1]])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}#{(src[1] + 'a'.ord).chr}#{8 - src[0] + i}"
+      elsif game.enemy?([src[0] - i, src[1]])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}x#{(src[1] + 'a'.ord).chr}#{8 - src[0] + i}"
+        break
+      end
+    end
+    result
+  end
+
+  def self.legal_moves_right(src, game)
+    result = []
+    (1..7).each do |i|
+      if !game.board.valid_coords?([src[0], src[1] + i])
+        break
+      elsif game.board.empty?([src[0], src[1] + i])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}#{(src[1] + i + 'a'.ord).chr}#{8 - src[0]}"
+      elsif game.enemy?([src[0], src[1] + i])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}x#{(src[1] + i + 'a'.ord).chr}#{8 - src[0]}"
+        break
+      end
+    end
+    result
+  end
+
+  def self.legal_moves_down(src, game)
+    result = []
+    (1..7).each do |i|
+      if !game.board.valid_coords?([src[0] + i, src[1]])
+        break
+      elsif game.board.empty?([src[0] + i, src[1]])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}#{(src[1] + 'a'.ord).chr}#{8 - src[0] - i}"
+      elsif game.enemy?([src[0] + i, src[1]])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}x#{(src[1] + 'a'.ord).chr}#{8 - src[0] - i}"
+        break
+      end
+    end
+    result
+  end
+
+  def self.legal_moves_left(src, game)
+    result = []
+    (1..7).each do |i|
+      if !game.board.valid_coords?([src[0], src[1] - i])
+        break
+      elsif game.board.empty?([src[0], src[1] - i])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}#{(src[1] - i + 'a'.ord).chr}#{8 - src[0]}"
+      elsif game.enemy?([src[0], src[1] - i])
+        result << "R#{(src[1] + 'a'.ord).chr}#{8 - src[0]}x#{(src[1] - i + 'a'.ord).chr}#{8 - src[0]}"
+        break
+      end
+    end
+    result
   end
 end
